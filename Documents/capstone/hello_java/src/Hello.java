575f5ef579869546b85cdf62334f79d800a2f97a
Benjamin Scott <66258096+benjascott@users.noreply.github.com>
2021-09-29 08:38:16 -0600

    reset dev

--- Documents/capstone/hello_java/src/Hello.java
+++ Documents/capstone/hello_java/src/Hello.java
@@ -3,8 +3,6 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.print("This is my ben/dev branch\n");
-        int numbers = 2 + 2;
-        System.out.println("Two plus two still equals " + numbers);
+        System.out.print("This is my dev branch\n");
     }
 }
